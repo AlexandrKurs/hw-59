4cbfc34051510ca7c168aa9f5920fa5d0e036cde
Alexandr Kurganov <alexandr.kurs.it@gmail.com>
2024-10-26 00:23:04 +0600

    Add form and types

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
-const App = () => {
+import React, { useState } from 'react';
+import { Movie } from './types';
+
+
+const App: React.FC = () => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const addMovie = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (inputValue) {
+      setMovies([...movies, { id: Date.now(), title: inputValue }]);
+      setInputValue('');
+    }
+  };
 
   return (
-    <>
-      Hello!
-    </>
-  )
-
+    <div>
+      <h2>Movie Tracker</h2>
+      <form onSubmit={addMovie}>
+        <input
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder="Введите название фильма"
+        />
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
 };
-export default App; 
+
+export default App;
